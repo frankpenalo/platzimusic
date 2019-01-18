c0890478d7e6ce27467744bcf9e31ba1f3deb066
frankpenalo <rototen@gmail.com>
2019-01-17 20:09:15 -0400

    Primer Commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,62 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    img(alt="Vue logo" src="./assets/logo.png")
+    h1 PlatziMusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" :value="country.value") {{ country.name }}
+    Spinner(v-show="loading")
+    ul
+      Artist(v-for="artist in artists" :artist="artist" :key="artist.mbid")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Artist from "./components/Artist.vue";
+import Spinner from "./components/Spinner";
+import getArtists from "./api";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      artists: [],
+      countries: [
+        { name: "Republica Dominicana", value: "dominican republic" },
+        { name: "Colombia", value: "colombia" },
+        { name: "Argentina", value: "argentina" },
+        { name: "España", value: "spain" }
+      ],
+      selectedCountry: "dominican republic",
+      loading: true
+    };
+  },
   components: {
-    HelloWorld
+    Artist,
+    Spinner
+  },
+  methods: {
+    refreshArtists() {
+      this.loading = true;
+      this.artists = [];
+      getArtists(this.selectedCountry).then(artists => {
+        this.loading = false;
+        this.artists = artists;
+      });
+    }
+  },
+  mounted() {
+    this.refreshArtists();
+  },
+  watch: {
+    selectedCountry: function() {
+      this.refreshArtists();
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Artist.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  li.artist
+    h2: a(:href="artist.url", target="_blank") {{ artist.name }}
+    img(:src="getImageUrl(artist.image)")
+</template>
+
+<script>
+export default {
+  name: "artist",
+  props: ["artist"],
+  methods: {
+    getImageUrl(imageList) {
+      for (const image of imageList) {
+        if (image.size === "large") {
+          return image["#text"];
+        }
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='stylus' scoped>
+li.artist
+  display block
+  margin 10px 0
+</style>
